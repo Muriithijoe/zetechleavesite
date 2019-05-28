b04a2d645af44e0d4e66c8131350ed6ea0d19959
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2019-05-28 14:10:03 +0300

    fixed bugs

--- zlive_project/settings.py
+++ zlive_project/settings.py
@@ -75,26 +75,26 @@ WSGI_APPLICATION = 'zlive_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.',
-#         'NAME': 'zeleave_db',
-#         'USER': 'root',
-#         'PASSWORD': 'BankRoll@7028',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'sql_mode': 'traditional',
-#         }
-#     }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zeleave_db',
+        'USER': 'root',
+        'PASSWORD': 'BankRoll@7028',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
+    }
+}
 
 
 # Password validation
